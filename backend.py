bb77226e56a207d3fd508c2c55c105ad185d2f41
CharanpreetSingh056 <lit2019056@iiitl.ac.in>
2021-07-07 02:51:30 +0530

    Added more routes

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 client=MongoClient('mongodb+srv://cirius:MVA1IzOr8GCYoSv8@cluster0.53e13.mongodb.net/db?retryWrites=true&w=majority')
 
-
+# computers data ko fetch karne ke lie
 @app.route('/computers',methods=['GET'])
 def computers():
     db=client.db
@@ -17,6 +17,7 @@ def computers():
         temp_list.append(all)
     return jsonify(temp_list)
 
+#users data ko fetch krne ke lie
 @app.route('/users',methods=['GET'])
 def users():
     db=client.db
@@ -27,6 +28,7 @@ def users():
         temp_list.append(all)
     return jsonify(temp_list)
 
+#github data ko fetch krne ke lie
 @app.route('/github',methods=['GET'])
 def github():
     db=client.db
@@ -37,6 +39,7 @@ def github():
         temp_list.append(all)
     return jsonify(temp_list)
 
+#sites data ko fetch krne ke lie
 @app.route('/sites',methods=['GET'])
 def sites():
     db=client.db
@@ -47,6 +50,7 @@ def sites():
         temp_list.append(all)
     return jsonify(temp_list)
 
+#github entries ko delete karne ke lie
 @app.route('/delete_github',methods=["POST"])
 def delete_github():
     url=request.json['url']
@@ -54,12 +58,45 @@ def delete_github():
     print(url)
     db=client.db
     all_github=db.github
-    print(all_github.find_one)
     all_github.find_one_and_delete({"url":url})
     print(all_github)
     return "Successful"
 
+#container=true ki saari keys return krne ke lie
+@app.route('/get_keys',methods=["GET"])
+def get_keys():
+    db=client.db
+    all_github=db.github
+    temp_array=all_github.find_one({'container':True})['keywords']
+    print(type(temp_array))
+    return jsonify(temp_array)
 
+#frontend se user credentials lene ke lie
+@app.route('/get_credentials',methods=["POST"])
+def get_credentials():
+    dc1=request.json['dc1']
+    dc2=request.json['dc2']
+    dc_name=request.json['dc_name']
+    username=request.json['username']
+    password=request.json['password']
+
+#github db mei keyword add krne ke lie
+@app.route('/add_keyword',methods=["POST"])
+def add_keyword():
+    keyword_to_add=request.json['keyword']
+    db=client.db
+    all_github=db.github
+    all_github.update_one({'container':True},{'$push': {'keywords': keyword_to_add}},True)
+    return "SUCCESS"
+
+#github db mei keyword delete krne ke lie
+@app.route('/remove_keyword',methods=["POST"])
+def delete_keyword():
+    keyword_to_delete=request.json['keyword']
+    db=client.db
+    all_github=db.github
+    all_github.update_one({'container':True},{'$pull': {'keywords': keyword_to_delete}},True)
+    return "SUCCESS"
 
 if __name__=='__main__':
     app.run(debug=True)
